a7edc8f624c6883ac71de7f3708ac2645e67edb4
gareth <gareth@gareth-laptop.(none)>
2012-10-19 21:00:59 +1100

    Restructure

---
+++ IO_util.f95
@@ -0,0 +1,133 @@
+MODULE IO_util
+    USE global_defs
+    IMPLICIT NONE
+    contains
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    PURE ELEMENTAL FUNCTION char_2_real(x)
+        ! Function to convert a character to a real, by 'reading' it
+        CHARACTER(len=charlen), INTENT(IN):: x
+        REAL(dp):: char_2_real
+
+        IF(len_trim(x)>0) THEN
+            read(x,*) char_2_real
+        ELSE
+            ! 'MISSING VALUE'
+            char_2_real=missing_value
+        END IF 
+    END FUNCTION
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    FUNCTION count_file_lines(input_file_unit_no)
+        ! Count the number of lines in input_file_unit_no
+        INTEGER(dp):: input_file_unit_no
+        INTEGER(dp):: count_file_lines
+
+        INTEGER(dp)::io_test=0
+
+        rewind(input_file_unit_no) ! Start of file
+
+        count_file_lines=0
+        DO WHILE (io_test>=0)
+            READ(input_file_unit_no,*, iostat=io_test)
+            IF(io_test>=0) count_file_lines=count_file_lines+1 
+        END DO
+
+        rewind(input_file_unit_no) ! Start of file
+    END FUNCTION
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    SUBROUTINE read_character_file(input_file_unit_no, output_lines, format_string)
+        ! Read the entire contents of input_file_unit_no into the allocatable
+        ! character array 'output_lines'. Use format_string in the read
+        INTEGER(dp), INTENT(IN):: input_file_unit_no
+        CHARACTER(*), INTENT(IN):: format_string
+        CHARACTER(len=charlen), ALLOCATABLE, INTENT(INOUT):: output_lines(:)
+
+        INTEGER(dp):: num_lines, i, io_test=0
+
+        ! Clean out 'output_lines' in case it is already allocated
+        if(allocated(output_lines)) deallocate(output_lines)
+
+        ! Count how many lines we will need
+        num_lines=count_file_lines(input_file_unit_no)
+        allocate(output_lines(num_lines))
+
+        print*, 'reading file ...' 
+        !stop
+        DO i=1,num_lines
+            read(input_file_unit_no, format_string, iostat=io_test) output_lines(i) 
+            !print*, trim(output_lines(i))
+        END DO
+        rewind(input_file_unit_no)
+
+    END SUBROUTINE
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    SUBROUTINE next_match(input_file_unit_no, pattern, io_test, format_string)
+        ! If input_file_unit_no is open for reading, advance to the next line
+        ! matching 'pattern'.
+        ! If no match, then return to the start of the file [io_test will be
+        ! returned negative in this case]
+        ! Assumes file is read with format=format_string, and io_test is used
+        ! to track IO errors
+        INTEGER(dp), INTENT(IN):: input_file_unit_no
+        INTEGER(dp), INTENT(OUT)::  io_test
+        CHARACTER(*), INTENT(IN):: pattern, format_string
+
+        CHARACTER(len=charlen) temp_char
+
+        temp_char='NOT'//pattern ! A string which will not = pattern
+
+        DO WHILE ( (trim(temp_char) .NE. pattern).AND.(io_test >=0) )
+            READ(input_file_unit_no, trim(format_string), iostat=io_test) temp_char
+        END DO
+       
+        IF(io_test<0) rewind(input_file_unit_no) 
+
+    END SUBROUTINE next_match
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    SUBROUTINE find_line_matches(lines, pattern, indices)
+        ! Find the indices in 'lines' (character vector of length 'n') which match with 'pattern'
+        ! FIXME: Presently, pattern must be at the start of the string
+        CHARACTER(len=charlen), INTENT(IN):: lines(:)
+        CHARACTER(*),INTENT(IN):: pattern
+        INTEGER(dp), allocatable, INTENT(OUT):: indices(:)
+
+        INTEGER(dp):: pos, i, n, counter
+        CHARACTER(len=charlen):: mychar
+
+        ! Remove indices if already exists
+        if(allocated(indices)) deallocate(indices)
+
+        n=len_trim(pattern) ! Length of pattern
+
+        ! Count occurrences of pattern (so we know size to allocate)
+        counter=0
+        DO i=1,size(lines)
+            mychar=lines(i)
+            IF(mychar(1:n)==pattern) THEN
+                counter=counter+1
+            END IF
+        END DO
+
+        IF(counter>0) THEN
+            ALLOCATE(indices(counter))
+            counter=0
+            DO i=1,size(lines)
+                mychar=lines(i)
+                IF(mychar(1:n)==pattern) THEN
+                    counter=counter+1
+                    indices(counter)=i
+                END IF
+            END DO
+        END IF
+            
+    END SUBROUTINE
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END MODULE IO_util
--- makefile
+++ makefile
@@ -1,12 +1,19 @@
 
-read_file: hecras_IO_b.o global_defs.o read_file.f95
+read_file: hecras_IO_b.o global_defs.o IO_util.o read_file.f95
 	gfortran $^ -o $@
 
-hecras_IO_b.o: global_defs.o river_classes.o hecras_IO_b.f95
+hecras_IO_b.o: global_defs.o IO_util.o river_classes.o hecras_IO_b.f95
 	gfortran -c hecras_IO_b.f95 
+
+IO_util.o: global_defs.o IO_util.f95
+	gfortran -c IO_util.f95
 
 river_classes.o: global_defs.o river_classes.f95
 	gfortran -c river_classes.f95 
 
 global_defs.o: global_defs.f95
 	gfortran -c $^
+
+
+clean:
+	rm *.o *.mod
